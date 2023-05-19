9f59922a65d9c66072678f21b16b60a1aaaf5c87
31 <roma.trebis@bk.ru>
2023-05-19 15:57:29 +0300

    Первая
    Первая

---
+++ Exercise1/Program.java
@@ -0,0 +1,50 @@
+package Exercise1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/* 
+ * Задание 1.
+ * 1. Даны два Deque представляющие два целых числа. Цифры хранятся в обратном порядке и каждый из их узлов содержит одну цифру.
+    1) Умножьте два числа и верните произведение в виде связанного списка.
+    2) Сложите два числа и верните сумму в виде связанного списка. Одно или два числа должны быть отрицательными
+ *
+*/
+
+public class Program {
+    public static void main(String[] args) {
+        Map<String, Set<String>> phoneBook = new HashMap<>();
+        
+        addNumber(phoneBook, "Иванов", "123-4567");
+        addNumber(phoneBook, "Петров", "234-5678");
+        addNumber(phoneBook, "Иванов", "345-6789");
+        addNumber(phoneBook, "Сидоров", "456-78910");
+        addNumber(phoneBook, "Петров", "567-891011");
+
+        Set<Map.Entry<String, Set<String>>> entries = phoneBook.entrySet();
+        List<Map.Entry<String, Set<String>>> list = new ArrayList<>(entries);
+        Collections.sort(list, (o1, o2) -> o2.getValue().size() - o1.getValue().size());
+
+        for (Map.Entry<String, Set<String>> entry : list) {
+            System.out.print(entry.getKey() + ": ");
+            for (String number : entry.getValue()) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void addNumber(Map<String, Set<String>> phoneBook, String name, String number) {
+        Set<String> numbers = phoneBook.get(name);
+        if (numbers == null) {
+            numbers = new HashSet<>();
+            phoneBook.put(name, numbers);
+        }
+        numbers.add(number);
+    }   
+}
